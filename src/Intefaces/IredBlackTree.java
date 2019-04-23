56a5f5695fef867c7f3b763c6d2ea00bedbede64
Dilan <Dilan@LAPTOP-6LVT0B6M>
2019-04-23 16:01:17 -0500

    //RED-BLACK TREE IN process

---
+++ src/Intefaces/IredBlackTree.java
@@ -0,0 +1,19 @@
+package Intefaces;
+
+import java.util.List;
+
+public interface IredBlackTree <T> {
+	
+	public  void insertar(T elemt);
+	public void eliminar (T elemt);
+	public List<T> getPreOrder();
+	public boolean existe(T elemt);
+    public T bsucar(T elemt);
+    public  redBlackNode<T> getRoot();
+    public int getWeight();
+    public int getHeight();
+    public T getMin();
+    public T getMax();
+	
+
+}
